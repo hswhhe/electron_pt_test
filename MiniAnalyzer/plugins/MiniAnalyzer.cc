5ac6a89a378ebd473c06a2df9ebf8fef8c5d33de
hswhhe <hsun3@uw.edu>
2022-07-04 21:11:46 +0200

    Test

---
+++ MiniAnalyzer/plugins/MiniAnalyzer.cc
@@ -0,0 +1,161 @@
+// -*- C++ -*-
+//
+// Package:    Test/MiniAnalyzer
+// Class:      MiniAnalyzer
+// 
+/**\class MiniAnalyzer MiniAnalyzer.cc Test/MiniAnalyzer/plugins/MiniAnalyzer.cc
+
+ Description: [one line class summary]
+
+ Implementation:
+     [Notes on implementation]
+*/
+//
+// Original Author:  
+//         Created:  Tue, 28 Jun 2022 00:17:48 GMT
+//
+//
+
+
+// system include files
+#include <memory>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/one/EDAnalyzer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "DataFormats/PatCandidates/interface/Electron.h"
+
+// Classes to save data
+#include "TTree.h"
+#include "TFile.h"
+#include<vector>
+//
+// class declaration
+//
+
+// If the analyzer does not use TFileService, please remove
+// the template argument to the base class so the class inherits
+// from  edm::one::EDAnalyzer<> and also remove the line from
+// constructor "usesResource("TFileService");"
+// This will improve performance in multithreaded jobs.
+
+class MiniAnalyzer : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
+   public:
+      explicit MiniAnalyzer(const edm::ParameterSet&);
+      ~MiniAnalyzer();
+
+      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+
+   private:
+      virtual void beginJob() override;
+      virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
+      virtual void endJob() override;
+
+      // ----------member data ---------------------------
+      edm::EDGetTokenT<pat::ElectronCollection> electronToken_;
+      TTree *mtree;
+      std::vector<float> electron_pt;
+};
+
+//
+// constants, enums and typedefs
+//
+
+//
+// static data member definitions
+//
+
+//
+// constructors and destructor
+//
+MiniAnalyzer::MiniAnalyzer(const edm::ParameterSet& iConfig):
+	 electronToken_(consumes<pat::ElectronCollection>(iConfig.getParameter<edm::InputTag>("electrons")))
+
+{
+   //now do what ever initialization is needed
+   //usesResource("TFileService");
+   edm::Service<TFileService> fs;
+   mtree = fs->make<TTree>("Events", "Events");
+   mtree->Branch("electron_pt",&electron_pt);
+   mtree->GetBranch("electron_pt")->SetTitle("electron transverse momentum");
+
+}
+
+
+MiniAnalyzer::~MiniAnalyzer()
+{
+ 
+   // do anything here that needs to be done at desctruction time
+   // (e.g. close files, deallocate resources etc.)
+
+}
+
+
+//
+// member functions
+//
+
+// ------------ method called for each event  ------------
+void
+MiniAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+   using namespace edm;
+   edm::Handle<pat::ElectronCollection> electrons; // add from this line
+   iEvent.getByToken(electronToken_, electrons);
+
+   electron_pt.clear();
+   for (const pat::Electron &el : *electrons) {
+        if (el.pt() < 5) continue;
+	electron_pt.push_back(el.pt());
+       	printf("electron with pt %4.1f, pass conversion veto %d\n",
+                el.pt(), el.passConversionVeto());
+   }
+
+
+
+#ifdef THIS_IS_AN_EVENT_EXAMPLE
+   Handle<ExampleData> pIn;
+   iEvent.getByLabel("example",pIn);
+#endif
+   
+#ifdef THIS_IS_AN_EVENTSETUP_EXAMPLE
+   ESHandle<SetupData> pSetup;
+   iSetup.get<SetupRecord>().get(pSetup);
+#endif
+   mtree->Fill();
+   return;
+}
+
+
+// ------------ method called once each job just before starting event loop  ------------
+void 
+MiniAnalyzer::beginJob()
+{
+}
+
+// ------------ method called once each job just after ending the event loop  ------------
+void 
+MiniAnalyzer::endJob() 
+{
+}
+
+// ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
+void
+MiniAnalyzer::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  //The following says we do not know what parameters are allowed so do no validation
+  // Please change this to state exactly what you do use, even if it is no parameters
+  edm::ParameterSetDescription desc;
+  desc.setUnknown();
+  descriptions.addDefault(desc);
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(MiniAnalyzer);
